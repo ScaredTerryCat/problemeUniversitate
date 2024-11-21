be57955a0e8c32113d802654aa7d2239aa19e3d4
ScaredTerryCat <mariohutu115@gmail.com>
2024-11-21 04:51:53 -0800

    Create myModules.cs

---
+++ c#/playingWithExtensionsC#/myModules.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Functions{
+	public static class intExtensions {
+	public static int estePar(this int number){
+	if(number%2==0){return 1;}return 0;
+	}
+};
+public static class stringExtensions{
+public static string estePar(this string str){return "par";}
+}
+}
