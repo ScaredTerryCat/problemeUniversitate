7f165bac2bad424a009fe8a5004610ef6fb92824
ScaredTerryCat <mariohutu115@gmail.com>
2024-12-18 08:10:26 -0800

    Update LaRusseMultiplication.cs

--- c#/LaRusseMultiplication.cs
+++ c#/LaRusseMultiplication.cs
@@ -8,10 +8,9 @@ public class HelloWorld
     public static long LaRusseMultiplication(int a,int b){
         long result=0;
         long B=b;
-        while(a>1){
-            a/=2;
-            B*=2;
+        while(a>0){
             if(a%2!=0){result+=B;}
+            a/=2;B*=2;
         }
     return result;}
     public static void Main(string[] args)
@@ -20,7 +19,7 @@ public class HelloWorld
         for(int i=0;i<10;i++){
             int a=rnd.Next(0,10);
             int b=rnd.Next(0,10);
-            int c=a*b;
+            long c=LaRusseMultiplication(a,b);
             Console.WriteLine($"\t{a} * {b} = {c}");
         }
     }
