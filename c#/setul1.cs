120659fa3ec9d0daabc422e0a95f1d286e1935e7
ScaredTerryCat <mariohutu115@gmail.com>
2025-02-15 10:58:37 -0800

    Update setul1.cs

--- c#/setul1.cs
+++ c#/setul1.cs
@@ -95,6 +95,119 @@ public static void problema6(){
     }
     catch(Exception e){Console.Write(e.Message);}
 }
+    public static void problema7(){
+			string eroare="eroare";
+			Console.Write("a= ");
+			if(!(int.TryParse(Console.ReadLine(),out int a))){Console.WriteLine(eroare);return;}
+			Console.Write("b= ");
+			if(!(int.TryParse(Console.ReadLine(),out int b))){Console.WriteLine(eroare);return;}
+			Console.WriteLine("Inainte:");
+			Console.WriteLine($"a= {a}  b= {b}");
+			Console.WriteLine("Dupa Swap:");
+			int aux=a;
+			a=b;
+			b=aux;
+			Console.WriteLine($"a= {a}  b= {b}");
+
+		}
+		public static void problema8(){
+			string eroare="eroare";
+			Console.Write("a=");
+			if(!(int.TryParse(Console.ReadLine(),out int a))){Console.WriteLine(eroare);return;}
+			Console.Write("b=");
+			if(!(int.TryParse(Console.ReadLine(),out int b))){Console.WriteLine(eroare);return;}
+			Console.WriteLine("Inainte de swap:");
+			Console.WriteLine($"a={a}  |  b={b}");
+			a=a+b;
+			b=a-b;
+			a=a-b;
+			Console.WriteLine("Dupa swap:");
+			Console.WriteLine($"a={a}  |  b={b}");
+			Console.WriteLine();
+		}
+		public static void problema9(){
+			Console.Write("n= ");
+			if(!(int.TryParse(Console.ReadLine(),out int n))){Console.WriteLine("Eroare.");return;}
+			Console.WriteLine($"Divizori {n} :");
+			for(int i=1;i*i<=n;i++){
+				if(n%i==0){
+					Console.WriteLine(i);
+					int d=n/i;
+					if(d!=i){Console.WriteLine(d);}
+				}
+			}
+		}
+		public static void problema10(){
+			Console.Write("n= ");
+			if(int.TryParse(Console.ReadLine(),out int n)){;} else{Console.WriteLine("Eroare.");return;}
+			bool prim=true;
+			if(n<2){prim=false;}
+			else if(n==2 || n==3){;}
+			else if(n%2==0 || n%3==0){prim=false;}
+			else{
+				for(int i=5;i*i<=n;i+=6){
+					if(n%i==0 || n%(i+2)==0){prim=false;break;}
+				}
+			}
+			Console.Write($"Numarul {n} ");
+			if(prim){
+				Console.Write("este prim.");
+			}
+			else{Console.Write("nu este prim.");}
+			return;
+		}
+		public static void problema11(){
+			Console.Write("n= ");
+			if(int.TryParse(Console.ReadLine(),out int n)){;}
+			else{Console.WriteLine("Eroare.");return;}
+			Console.Write($"Cifrele lui n={n} in ordine inversa:");
+			if(n==0){Console.Write(0);return;}
+			while(n!=0){
+				int cifra=n%10;
+				Console.Write($" {cifra}");
+				n/=10;
+			}
+		}
+		public static void problema12(){
+			//cate numere intregi divizibile cu n se afla in intervalul [a,b]
+			string sa,sb,sn;
+			Console.Write("n= ");
+			sn=Console.ReadLine();
+			Console.Write("a= ");
+			sa=Console.ReadLine();
+			Console.Write("b= ");
+			sb=Console.ReadLine();
+			if(int.TryParse(sn,out int n) && int.TryParse(sa,out int a) && int.TryParse(sb,out int b)){
+			;
+			}
+			else{Console.Write("Eroooare.");return;}
+			Console.Write($"Divizori ai lui {n} in intervalul [{a},{b}] :\t");
+			for(int i=1;i*i<=n;i++){
+				if(n%i==0){
+					if(i>=a && i<=b){
+					Console.Write($" {i}");
+				}
+				int d=n/i;
+				if(d>=a && d<=b && d!=i){Console.Write($" {d}");}
+
+				}
+			}
+		}
+		public static void problema13(){
+			string sy1,sy2;
+			Console.Write("An y1= ");
+			sy1=Console.ReadLine();
+			Console.Write("An y2= ");
+			sy2=Console.ReadLine();
+			if(int.TryParse(sy1,out int y1) && int.TryParse(sy2,out int y2)){;}else{Console.WriteLine("Eroare.");return;}
+			Console.Write($"Ani bisecti intre {y1} si {y2}:");
+			for(int i=y1;i<=y2;i++){
+				bool bisect=true;
+				if(i%4!=0){bisect=false;}
+				else if(i%100==0 && i%400!=0){bisect=false;}
+				if(bisect){Console.Write($" {i}");}
+			}
+		}
 
 }
 public class Program
